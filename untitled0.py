b00665f859968bedde2fc70e1165c9e874191f57
volty69 <94720823+volty69@users.noreply.github.com>
2021-11-20 03:53:27 +0530

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -19,5 +19,23 @@ WordsSeparated()
 
 #2 Read a text file and display the number of vowels/consonants/uppercase/lowercase characters in the file
 
-def vcul():
-    f=open("Report.txt")
+"""def vcul():
+    f=open("abc.txt","r")
+    V=C=U=L=0
+    data=f.read()
+    for i in data:
+        if i.isalpha():
+            if i.isupper():
+                U+=1
+            if i.islower():
+                L+=1
+            if i.lower() in 'aeiou':
+                V+=1
+            else:
+                C+=1
+    print("Vowels = ",V)
+    print("Consonants = ",C)
+    print("Uppercase =",U)
+    print("Lowercase",L)
+vcul()
+"""
